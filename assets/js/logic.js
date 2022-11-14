ef99f030223ba64494e12f6f3a5502e65b3b7a18
Siddhant Patil <shimpli.patil2804@gmail.com>
2022-11-14 06:59:14 +0530

    Add files via upload

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,12 +4,22 @@ function search_dsa() {
 
   input = input.toLowerCase();
 
+  document.getElementById("gyan").style.display = "none";
+
   let x = document.getElementsByClassName("dsa");
+  let flag = 0;
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
       x[i].style.display = "none";
     } else {
+      flag = 1;
       x[i].style.display = "list-item";
     }
+  }
+
+  if (flag == 0) {
+    x.style.display = "none";
+    document.getElementById("gyaan").innerHTML =
+      "<h3>OPPS! No Search realted to this.</h3>";
   }
 }
