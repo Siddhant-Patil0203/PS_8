cbeffdbbb73022fbfd94a194ec4a4398c8e78762
Siddhant Patil <shimpli.patil2804@gmail.com>
2022-11-14 06:40:25 +0530

    Add files via upload

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+//Main Logic
+function search_dsa() {
+  let input = document.getElementById("searchbar").value;
+
+  input = input.toLowerCase();
+
+  let x = document.getElementsByClassName("dsa");
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
